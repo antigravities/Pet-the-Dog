ccdf7cf4d1f7bd6ac0cab8099cb52cf5ec977f05
Alexandra Frock <im@alexandra.moe>
2020-08-06 00:41:08 -0400

    now with more dog punching
    "well, the melee animation can be mistaken for a 'petting the dog' animation"

--- main.lua
+++ main.lua
@@ -5,13 +5,22 @@ Mannequin:Import("EUIAnchor")
 Mannequin:Import("Vector")
 Mannequin:Import("UIOverlay")
 Mannequin:Import("Emote")
+Mannequin:Import("Job")
+Mannequin:Import("EJobResult")
 
 print("Get ready to pet the dog!")
 
 local btn
 
 function pet()
-    UIOverlay.SelectedAgent.Body.ShowEmote(Emote("emotes/happy"), 5000)
+    local sel = UIOverlay.SelectedAgent
+
+    local job = Job("PathToAgent", sel.Scene, sel)
+    job:Dispatch(sel.Scene, function(status)
+        if status == EJobResult.Success then sel.Body.ShowEmote(Emote("emotes/happy"), 5000)
+        else print("path job failed " .. status)
+        end
+    end)
 end
 
 Event.Once("Game", function()
@@ -24,6 +33,7 @@ end)
 Event.On("Tick", function(ticks)
    if ticks%10 == 0 and not (btn == nil) then
         local sel = UIOverlay.SelectedAgent
+        if not (sel == nil) then print(sel.Data.ID) end
         btn.Visible = not (sel == nil) and sel.IsLiving and sel.Data.ID == "dom_dog"
    end
 end)
